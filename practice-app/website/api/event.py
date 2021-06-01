9c051b6687c9a2bfc3419eccc400ca61b4950525
BERK <atilberk98@gmail.com>
2021-06-01 16:16:00 +0300

    file structure is changed

--- practice-app/website/api/event.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, request,  url_for, jsonify, make_response, abort
-
-events = Blueprint('events', __name__)
-
-@events.route('events/', methods = ['POST'])
-def create_event():
-    return jsonify({'event': {"name": "Temp"}}), 201
