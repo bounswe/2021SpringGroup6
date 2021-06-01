708ef817042587f676f29594539febddf747f928
fileames <elifsemabalcioglu@gmail.com>
2021-06-01 17:29:43 +0300

    Created Event Model
    Database model for events and a function to make the model JSON is created.

--- practice-app/website/models.py
+++ practice-app/website/models.py
@@ -6,3 +6,21 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    date = db.Column(db.Text)
+    location = db.Column(db.String())
+    creator_user = db.Column(db.String(), db.ForeignKey('user.id'))
+
+    def serialize(self):
+       """Return object data in JSON serializable format"""
+       return {
+           'id'             : self.id,
+           'name'           : self.name,
+           'date'           : self.date,
+           'location'       : self.location,
+           'creator_user'   : self.creator_user
+       }
+
