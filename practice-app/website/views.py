eba427260c8e3eda410e067771667164637e8a38
fileames <elifsemabalcioglu@gmail.com>
2021-06-03 17:07:59 +0300

    Create Event Front-end
    Create event form created with dropdown.
    For dropdown with text filter bootstrap-select added.

--- practice-app/website/views.py
+++ practice-app/website/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
+import requests
 
 views = Blueprint('views', __name__)
 
@@ -7,4 +8,27 @@ views = Blueprint('views', __name__)
 @login_required
 def home():
     return render_template("home.html", user=current_user)
+
+
+def get_sport_names():
+    uri = 'https://www.thesportsdb.com/api/v1/json/1/all_sports.php'
+
+    r = requests.get(uri)
+    
+    result = r.json()
+
+    sports={}
+
+    for sport in result['sports']:
+        sports[sport['idSport']] = sport['strSport']
+    return sports
+
+
+
+@views.route('create_event/')
+@login_required
+def create_event():
+    sports = get_sport_names()
+    return render_template("create_event.html", user= current_user, sports=sports)
+
 
