3165c8dc7c1c59e9ee40b8302efac97cb2ae66bb
fileames <elifsemabalcioglu@gmail.com>
2021-06-04 12:30:46 +0300

    Documentation Template Created
    - flasgger will be used
    - some paths are corrected

--- practice-app/website/api/event.py
+++ practice-app/website/api/event.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, request,  url_for, jsonify, make_response, abort
+from flasgger.utils import swag_from
 
 events = Blueprint('events', __name__)
 
 @events.route('events/', methods = ['POST'])
+@swag_from('doc/events_POST.yml', methods=['POST'])
 def create_event():
     return jsonify({'event': {"name": "Temp"}}), 201
