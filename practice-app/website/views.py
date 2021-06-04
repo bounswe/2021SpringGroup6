03b9fbec71942cb8eed8696da72ca51f6c14d8cf
fileames <elifsemabalcioglu@gmail.com>
2021-06-04 17:23:55 +0300

    Connect Front-end

--- practice-app/website/views.py
+++ practice-app/website/views.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify, request, flash, redirect, url_for
 from flask_login import login_required, current_user
 import requests
+import json
 
 views = Blueprint('views', __name__)
 
@@ -25,10 +26,33 @@ def get_sport_names():
 
 
 
-@views.route('create_event/')
+@views.route('create_event/', methods=['POST','GET'])
 @login_required
 def create_event():
     sports = get_sport_names()
+    if request.method == 'POST':
+        event = {
+            "name" : request.form.get('name'),
+            "date" : request.form.get('date'),
+            "location" : request.form.get('location'),
+            "sport" : request.form.get('sport'),
+            "creator_user" : current_user.id
+        }
+
+        req = "http://127.0.0.1:5000/api/v1.0/events"
+        headers = {'Content-type': 'application/json'}
+        response = requests.post(req, data=json.dumps(event), headers=headers)
+        result = response.content
+
+        if response.status_code == 201:
+            flash('Event Created', category='success')
+            # TODO: When event page implemented, redirect to it.
+            return redirect(url_for('views.home'))
+        elif response.status_code == 400 :
+            flash('Check Information Entered', category='error')
+        else:
+            flash('Error Occured, Try Again Later', category='error')
+    
     return render_template("create_event.html", user= current_user, sports=sports)
 
 
