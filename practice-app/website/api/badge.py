0dc07f92197c43367c06b1d9e5d51136b793fc63
batil <atilberk98@gmail.com>
2021-06-08 01:36:05 +0300

    badge post function structure is changed

--- practice-app/website/api/badge.py
+++ practice-app/website/api/badge.py
@@ -7,6 +7,9 @@ from flasgger.utils import swag_from
 
 badges = Blueprint('badges',__name__)
 
+def extract_point(price):
+    return str(price).split('.')[1][3]
+
 def get_badge_point():
     """
     This is a function to assign a point to the badge.
@@ -16,7 +19,14 @@ def get_badge_point():
     headers = {'X-CoinAPI-Key' : '206D8179-A9A3-4C59-860B-84C1E511EB55'}
     response = requests.get(url, headers=headers)
 
-    return str(response.json()['rate']).split('.')[1][3]
+    return extract_point(response.json()['rate'])
+
+def prepare_badge(data):
+    badge_name = data['name']
+    badge_symbol = data['symbol']
+    badge = Badge(name=badge_name, symbol=badge_symbol)
+
+    return badge
 
 @badges.route('/',methods = ['POST'])
 @swag_from('doc/badges_POST.yml')
@@ -27,11 +37,9 @@ def add_badge():
     If the badge is added successfully, it returns 201 response code.
     If there is a problem during the addition of the badge, it returns 403 response code.
     """
-    result = request.json
-    badge_name = result['name']
-    badge_point = get_badge_point()
-    badge_symbol = result['symbol']
-    badge = Badge(name=badge_name, symbol=badge_symbol, point=badge_point)
+
+    badge = prepare_badge(request.json)
+    badge.badge_point = get_badge_point()
     try:
         db.session.add(badge)
         db.session.commit()
