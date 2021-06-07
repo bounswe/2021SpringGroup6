410f9659bce32b5217662bac985e2548286adfcf
omersuve <omersuve@gmail.com>
2021-06-07 20:18:23 +0300

    commenting on equipment api

--- practice-app/website/api/equipment.py
+++ practice-app/website/api/equipment.py
@@ -9,15 +9,19 @@ from flasgger.utils import swag_from
 
 equipments = Blueprint('equipments', __name__)
 
+# returns the arbitrary price for equipment using an API
 def get_price():
     url = 'https://api.blockchain.com/v3/exchange/tickers'
     response = requests.get(url)
     return response.json()[0]['price_24h']
 
-
+# endpoint of the api of equipments
+# applies a Post operation
+# documentation for Post in .yml file
 @equipments.route('/', methods=['POST'])
 @swag_from('doc/equipments_POST.yml', methods=['POST'])
 def post_equipment():
+    # checking parameters
     if not request.json or not 'name' in request.json:
         return "Parameter not correct", 400
 
@@ -26,6 +30,10 @@ def post_equipment():
     price = get_price()
 
     tempEquipment = Equipment.query.filter_by(name=equipment_name).first()
+    
+    # if equipment exists, returns code 409
+    # handling error if an error occurs on Sqlalchemy
+    # returns 201 if everything is fine
     if tempEquipment:
         return 'Equipment already exists!', 409
     else:
