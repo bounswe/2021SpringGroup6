87ae391a76820ae62627548a1db0a2b100302a2d
omersuve <omersuve@gmail.com>
2021-06-08 21:31:23 +0300

    equipment test added

--- practice-app/website/test/equipment_test.py
+++ practice-app/website/test/equipment_test.py
@@ -1,27 +1,13 @@
 import unittest
-import requests, json
+from ..api.equipment import get_price
+from ..models import Equipment
 
-class TestEquipmentAPI(unittest.TestCase):
+class TestEventAPI(unittest.TestCase):
 
-    def setUp(self):
-        self.req = "http://127.0.0.1:5000/api/v1.0/equipments"
-        self.headers = {'Content-type': 'application/json'}
+    def test_price(self):
+        """
+            Price a string which has value of 0.0
+        """
+        price_value = get_price()
 
-    def test_equipment_POST(self):
-
-        # Equipment named Ball
-        Equipment ={
-            'name': 'Ball'
-        }
-        response = requests.post(self.req, data=json.dumps(Equipment), headers=self.headers)
-        self.assertEqual(response.status_code, 201)
-
-    def test_equipment_POST_empty_name(self):
-
-        # Name field is missing
-        Equipment ={
-            'name': ''
-        }
-
-        response = requests.post(self.req, data=json.dumps(Equipment), headers=self.headers)
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(price_value, "0.0")
