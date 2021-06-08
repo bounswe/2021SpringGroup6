b3b2115d32b5ecf2e99abbefc1e00a21ecb4f4cf
fileames <elifsemabalcioglu@gmail.com>
2021-06-08 08:34:07 +0300

    Comments Updated

--- practice-app/website/test/sport_test.py
+++ practice-app/website/test/sport_test.py
@@ -9,24 +9,29 @@ class TestSportAPI(unittest.TestCase):
         # mock data
         self.data = [Sport(id = 102, sport = "Football"), Sport(id = 103, sport = "Basketball"), Sport(id = 105, sport = "Tennis")]        
 
-    # Test with keyword "ball"
     def test_sport_keyword(self):
+        """
+            Given keyword "ball", sports that contain that keyword should be returned.
+        """
 
         ball_keyword = get_sport_by_keyword(self.data, "ball")
         expected = [self.data[0], self.data[1]] # Only "Football" and "Basketball"
 
         self.assertEqual(ball_keyword, expected)
 
-    # Test with no keyword
     def test_no_keyword(self):
-        
+        """
+            Given no keyword, all sports should be returned.
+        """
         no_keyword = get_sport_by_keyword(self.data, "")
         expected = self.data # No filtering
 
         self.assertEqual(no_keyword, expected)
     
-    # Test with no result, no sport with "aa"
     def test_no_result(self):
+        """
+            Given a keyword not existing on sports, no sport should be returned.
+        """
 
         no_data = get_sport_by_keyword(self.data, "aa")
         expected = [] # No match
