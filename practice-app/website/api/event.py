3d96a54d7789ef3b6bf3b18932c42ebd657fdba3
Musa <musaihtiyar@gmail.com>
2021-06-08 16:50:53 +0300

    Due to a merge occured in Github, some inconsistencies have arrised. Newly added points are added to the branch to avoid conflicts

--- practice-app/website/api/event.py
+++ practice-app/website/api/event.py
@@ -12,7 +12,6 @@ from sqlalchemy import exc
 from sqlalchemy.sql.elements import Null
 events = Blueprint('events', __name__)
 
-
 API_KEY = '<api_key_coordinates>'
 API_KEY2 = '<api_key_weather>'
 
@@ -23,41 +22,80 @@ def get_weather(latitude, longitude):
     r = r.json()
     return r["weather"][0]["description"], r["weather"][0]["icon"]
 
-
-
+"""
+    Get coordinates using Google Maps API
+    parameters:
+        address: "address of the location" 
+    return:
+        True if valid False otherwise
+"""
 def getCoordinates(address):
-    parameters = {'key': API_KEY, 'address': address}
+
+    # Make GET request to the uri using API_KEY
+    parameters = {'key': API_KEY , 'address': address}
     uri = 'https://maps.googleapis.com/maps/api/geocode/json'
 
     r = requests.get(uri, params=parameters)
-
+    
     result = r.json()
-    print(result)
 
+    # If there is no error return information from response.
     if r.status_code == 200:
         if result['status'] == "OK":
             formatted_address = result['results'][0]['formatted_address']
             longitude = result['results'][0]['geometry']['location']['lng']
             latitude = result['results'][0]['geometry']['location']['lat']
             return formatted_address, longitude, latitude, "OK"
-
-    if result['status'] == "ZERO_RESULTS" or result['status'] == "INVALID_REQUEST":
-        return "", 0, 0, "Address Not Valid"
-    elif result['status'] == "OVER_DAILY_LIMIT" or result['status'] == "OVER_QUERY_LIMIT" or result['status'] == "REQUEST_DENIED" or result['status'] == "UNKNOWN_ERROR":
-        return "", 0, 0, "Try Later"
+    
+    # Address not valid, error messages from API docs
+    if result['status'] == "ZERO_RESULTS" or result['status'] == "INVALID_REQUEST": 
+        return "",0,0,"Address Not Valid"
+    # Cannot make a request
+    elif result['status'] == "OVER_DAILY_LIMIT" or result['status'] == "OVER_QUERY_LIMIT" or result['status'] == "REQUEST_DENIED" or result['status'] == "UNKNOWN_ERROR": 
+        return "",0,0,"Try Later"
+    # Remaining Errors
     else:
-        return "", 0, 0, "Try Later"
+        return "",0,0,"Try Later"
 
 
-@events.route('/', methods=['GET', 'POST'])
+"""
+    Check the validity of the sport field of event
+    parameters:
+        new_event: Event 
+    return:
+        True if valid False otherwise
+"""
+def check_event_sport(new_event):
+    try:
+        # sport Ids between 102-120
+        if int(new_event.sport) < 102 or int(new_event.sport) > 120:
+            return False
+        return True
+    except:
+        # sport string cannot be changed to integer
+        return False
+
+"""
+    Check the format of the date field of event
+    Format should match YYYY-MM-DDTHH:MM
+    parameters:
+        new_event: Event 
+    return:
+        True if valid False otherwise
+"""
+def check_event_date(new_event):
+    # Date format YYYY-MM-DDTHH:MM
+    date_regex = "^(20[0-9][0-9])-(0[1-9]|1[0-2])-(0[1-9]|1[0-9]|2[0-9]|3[0-1])T(0[0-9]|1[0-9]|2[0-3]):(0[0-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]2|5[0-9])$"
+    if not re.match(date_regex, new_event.date):
+        return False
+    return True
+
+
+@events.route('/', methods = ['GET','POST'])
 @swag_from('doc/events_POST.yml', methods=['POST'])
 @swag_from('doc/events_GET.yml', methods=['GET'])
 def event():
-
-
-
     # handles get request
-
     if request.method == 'GET':
         # fethes body parameters
         query_parameters = request.args
@@ -113,43 +151,102 @@ def event():
         
 
     if request.method == 'POST':
-        if not request.json or not 'name' in request.json or not 'creator_user' in request.json or not 'location' in request.json or not 'sport' in request.json:
-            return "Parameters not correct", 400
+        """
+            Used to create a new event.
+            Endpoint description:
+                ./api/v.10/events
+                'POST':
+                    JSON Request Body Format : {
+                                                name = "Name of the event, title." required,
+                                                creator_user = "Id of the user creating the event. Id must be registered to a user." required,
+                                                location = "Address of the event, given using basic English." required,
+                                                sport = "Id of the sport. Between 102-120." required,
+                                                date ="Date of the event. Format is "YYYY-MM-DDTHH:MM" required
+                                            }
+                    Response Example : {
+                                            "creator_user": 1,
+                                            "date": "10.12.2021",
+                                            "entered_address": "Trabzon",
+                                            "formatted_address": "Trabzon, Ortahisar/Trabzon, Turkey",
+                                            "id": 2,
+                                            "latitude": 41.0026969,
+                                            "longitude": 39.7167633,
+                                            "name": "abc",
+                                            "sport": 103
+                                        }
+                    Status Codes:
+                        201: "Event created and added to database."
+                        400: "Body parameters are not correct."
+                        403:  "There is an error, try later."
+        """
 
-        formatted_address, longitude, latitude, error = getCoordinates(
-            request.json['location'])
+        # All parameters must be present.
+        if not request.json or not 'name' in request.json or not 'creator_user'  in request.json or not 'location' in request.json or not 'sport' in request.json or not 'date' in request.json:
+            return jsonify({"error":"Parameters not correct"}), 400
 
+        # Get coordinates using Google Maps API.
+        formatted_address, longitude, latitude, error = getCoordinates(request.json['location'])
+
+        # Return error if fetch was not correct.
         if error != "OK":
             if error != "Try Later":
-                return error, 400
+                return jsonify({"error": error}), 400
             else:
-                # TODO: If api not responding or full quota add without address fields
-                return "Try Later", 403
+                return jsonify({"error": "Service Unavailable"}), 503
 
+        # Create database model
         new_event = Event(
-            name=request.json['name'],
-            date=request.json['date'] if 'date' in request.json else None,
-            formatted_address=formatted_address,
-            entered_address=request.json['location'],
-            longitude=longitude,
-            latitude=latitude,
-            creator_user=request.json['creator_user'],
-            sport=request.json['sport']
+            name = request.json['name'],
+            date = request.json['date'],
+            formatted_address = formatted_address,
+            entered_address = request.json['location'],
+            longitude = longitude,
+            latitude = latitude,
+            creator_user = request.json['creator_user'],
+            sport = request.json['sport']
         )
 
+        # Check if creator_user is valid and there is a user registered with that id.
+        user = User.query.get(request.json['creator_user'])
+        if not user:
+            return jsonify({"error":"User Not Registered"}), 400
+
+        # Check sport id.
+        if not check_event_sport(new_event):
+            return jsonify({"error":"Sport Id Is Not Correct"}), 400
+
+        # Check date format.
+        if not check_event_date(new_event):
+            return jsonify({"error":"Date Format Not Correct"}), 400
+
+
         try:
+            # Add Event to database.
             db.session.add(new_event)
             db.session.commit()
-        except exc.NoReferenceError as e:
-            db.session.rollback()
-            return "User Not Registered", 400
         except exc.SQLAlchemyError as e:
+            # In case of error, return error message
             db.session.rollback()
-            return "Try Later", 403
-
+            return jsonify({"error": "Service Unavailable"}), 503
+        
+        # No error, return new event information
         return jsonify(new_event.serialize()), 201
 
 
+@events.route('/<event_id>/', methods = ['GET'])
+@swag_from('doc/event_GET.yml', methods=['GET'])
+def get_event_by_id(event_id):
+    if request.method == 'GET':
+        event = Event.query.get(event_id)      
+        event_with_weather = event.serialize()
+        event_with_weather["hour"] =  event_with_weather["date"][11:]
+        event_with_weather["date"] = event_with_weather["date"][:10]     
+        weather, weather_icon = get_weather(event_with_weather["latitude"], event_with_weather["longitude"])
+        event_with_weather["weather"] = weather
+        event_with_weather["weather_icon"] = weather_icon
+        sport_names = get_sport_names()       
+        event_with_weather["sport"] = sport_names[str(event_with_weather["sport"])]                
+        return jsonify(event_with_weather), 200
 
 # When the id of the event given, corresponding discussion is returned by adding the definition of the sport type in the json format
 # Corresponding event must exist and have a sport type in db.
@@ -214,19 +311,3 @@ def discussionForEvent(event_id):
 
         result = {"id": event_id, "description": description, "text": text}
         return jsonify(result), 201
-
-@events.route('/<event_id>/', methods = ['GET'])
-@swag_from('doc/event_GET.yml', methods=['GET'])
-def get_event_by_id(event_id):
-    if request.method == 'GET':
-        event = Event.query.get(event_id)      
-        event_with_weather = event.serialize()
-        event_with_weather["hour"] =  event_with_weather["date"][11:]
-        event_with_weather["date"] = event_with_weather["date"][:10]     
-        weather, weather_icon = get_weather(event_with_weather["latitude"], event_with_weather["longitude"])
-        event_with_weather["weather"] = weather
-        event_with_weather["weather_icon"] = weather_icon
-        sport_names = get_sport_names()       
-        event_with_weather["sport"] = sport_names[str(event_with_weather["sport"])]                
-        return jsonify(event_with_weather), 200
-
---
+++ practice-app/website/test/event_test.py
@@ -0,0 +1,79 @@
+import unittest
+from ..api.event import check_event_date, check_event_sport
+from ..models import Event
+
+class TestEventAPI(unittest.TestCase):
+
+    def test_date_correct_format(self):
+        """
+            Given a string with correct format as YYYY-MM-DD, returns True
+        """
+        event = Event(date = "2021-12-30T12:30")
+
+        is_date_correct = check_event_date(event)
+
+        self.assertEqual(is_date_correct, True)
+
+
+    def test_date_invalid_month(self):
+        """
+            Given a string with a non existing month such as 13 returns False
+        """
+        event = Event(date="2021-13-30T12:30")
+
+        is_date_correct = check_event_date(event)
+
+        self.assertEqual(is_date_correct, False)
+
+
+    def test_date_invalid_hour(self):
+        """
+            Given a string with a non existing hour such as 25 returns False
+        """
+        event = Event(date = "2021-11-30T25:30")
+
+        is_date_correct = check_event_date(event)
+
+        self.assertEqual(is_date_correct, False)
+
+
+    def test_date_invalid_format(self):
+        """
+            Given a string with a different format returns False
+        """
+        event = Event(date = "2021-11-30")
+
+        is_date_correct = check_event_date(event)
+
+        self.assertEqual(is_date_correct, False)
+
+
+    def test_sport_correct(self):
+        """
+            Given sport id between 102-120 returns True
+        """
+        event = Event(sport = "104")
+
+        is_sport_correct = check_event_sport(event)
+
+        self.assertEqual(is_sport_correct, True)
+
+    def test_sport_out_of_range(self):
+        """
+            Given sport id not between 102-120 returns False
+        """
+        event = Event(sport = "5")
+
+        is_sport_correct = check_event_sport(event)
+
+        self.assertEqual(is_sport_correct, False)
+
+    def test_sport_incorrect_type(self):
+        """
+            Given sport id as a string that can be changed to a integer returns False
+        """
+        event = Event(sport = "five")
+
+        is_sport_correct = check_event_sport(event)
+
+        self.assertEqual(is_sport_correct, False)
