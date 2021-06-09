b36f3fe48c3fa07c1b43f06357694aa554c584fa
omersuve <omersuve@gmail.com>
2021-06-09 20:44:23 +0300

    Update auth.py

--- practice-app/website/api/auth.py
+++ practice-app/website/api/auth.py
@@ -56,7 +56,7 @@ def sign_up():
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
-            flash('Successfull', category='success')
+            flash('Successful', category='success')
             return redirect(url_for('views.home'))
 
     return render_template("sign_up.html", user=current_user)
