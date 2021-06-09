8a4e2a509ff639e498b579fdb30c1dbe59ca3260
Can Atakan Uğur <56476673+bnbcfyh@users.noreply.github.com>
2021-06-09 12:07:21 +0300

    Update post_comment_test.py

--- practice-app/website/test/post_comment_test.py
+++ practice-app/website/test/post_comment_test.py
@@ -5,7 +5,13 @@ from ..api.event import check_comment_has_text
 class TestEventAPI(unittest.TestCase):
 
     # given an empty comment, it returns false
-    def test_comment_has_test(self):
+    def test_comment_empty(self):
         comment = ""
         result = check_comment_has_text(comment)
         self.assertEqual(result, False)
+
+    # given any comment that has text, it returns true
+    def test_comment_has_text(self):
+        comment = "When does the event start?"
+        result = check_comment_has_text(comment)
+        self.assertEqual(result, True)
