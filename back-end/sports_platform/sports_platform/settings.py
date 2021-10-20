d9378c14ce4423f2368a6013ab64fad2546872db
batil <atilberk98@gmail.com>
2021-10-20 22:58:27 +0300

    delete secret key

--- back-end/sports_platform/sports_platform/settings.py
+++ back-end/sports_platform/sports_platform/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ynez83zqi9ee2ln@32!(=z_&vvo)x^&cee0rw^ny8uhl-kr8qi'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
