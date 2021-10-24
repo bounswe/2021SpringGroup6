0d225c2018300b396bb38998fa492ec8b9edf3a9
fileames <elifsemabalcioglu@gmail.com>
2021-10-24 21:58:52 +0300

    User and Sport models
    I added the fields for Email, surname and sport, skill level connection.

--- back-end/sports_platform/sports_platform_api/models/user_models.py
+++ back-end/sports_platform/sports_platform_api/models/user_models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email = models.EmailField()
+    surname = models.CharField()
+
+class Sport_SkillLevel(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    sport = models.IntegerField()
+    skill_level = models.IntegerField()
