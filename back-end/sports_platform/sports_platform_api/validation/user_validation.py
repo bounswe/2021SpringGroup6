9ffd7035f1a5a6f2ea7f392226d6fed8f8236a9e
fileames <elifsemabalcioglu@gmail.com>
2021-10-24 22:16:11 +0300

    Validation For Sport-Skill Level

--- back-end/sports_platform/sports_platform_api/validation/user_validation.py
+++ back-end/sports_platform/sports_platform_api/validation/user_validation.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from generic_validators import english_dot
 
-
+class Sport_SkillLevel(serializers.Serializer):
+    sport = serializers.IntegerField(min_value = 0, required=True)
+    skill_level = serializers.IntegerField(min_value = 1, max_value = 5, required=True)
 
 class User(serializers.Serializer):
    email = serializers.EmailField(required=True)
    surname = serializers.CharField(min_length = 2, max_length = 30, validators = [english_dot])
+   sports = serializers.ListField(child=Sport_SkillLevel())
