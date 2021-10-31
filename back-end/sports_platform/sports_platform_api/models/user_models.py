75ab689db26996c57389113033f3ca051726e498
batil <atilberk98@gmail.com>
2021-10-31 18:09:53 +0300

    update user models

--- back-end/sports_platform/sports_platform_api/models/user_models.py
+++ back-end/sports_platform/sports_platform_api/models/user_models.py
@@ -1,10 +1,22 @@
 from django.db import models
 
 class User(models.Model):
-    email = models.EmailField()
-    surname = models.CharField()
+   user_id = models.BigAutoField(primary_key=True)
+   email = models.CharField(max_length=60)
+   password = models.CharField(max_length=30)
+   identifier = models.CharField(max_length=30)
+   name = models.CharField(max_length=30,blank=True)
+   familyName = models.CharField(max_length=30,blank=True)
+   birthDate = models.DateField(blank=True, null=True)
+   gender = models.SmallIntegerField(blank=True, null=True)
 
-class Sport_SkillLevel(models.Model):
+
+class Sport(models.Model):
+    sport_id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+
+
+class SportSkillLevel(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    sport = models.IntegerField()
-    skill_level = models.IntegerField()
+    sport_id = models.ForeignKey(Sport, on_delete=models.CASCADE)
+    skill_level = models.SmallIntegerField()
