5e740b661d241696344fc736d11bc4b9b3803d37
batil <atilberk98@gmail.com>
2021-11-01 15:43:08 +0300

    table name added

--- back-end/sports_platform/sports_platform_api/models/sport_models.py
+++ back-end/sports_platform/sports_platform_api/models/sport_models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
 class Sport(models.Model):
+    class Meta:
+        db_table = 'sport'
+
     sport_id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=30)
