4dd6765d6f9bf8f8e9946137415eadbaa661d363
Musa <musaihtiyar@gmail.com>
2021-11-02 21:56:03 +0300

    Header part has been prepared.

--- front-end/playground/src/App.js
+++ front-end/playground/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './PermanentComponents/Header';
+
+ 
 
 function App() {
   return (
     <div>
-      <header >
-        
-      </header>
+      <Header />
     </div>
   );
 }
