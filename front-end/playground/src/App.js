ad31130523ee58bbd82e521f81ccfa45d2745bab
Musa <musaihtiyar@gmail.com>
2021-11-03 21:41:34 +0300

    Footer element which contains contact information as an example has been added to the system.

--- front-end/playground/src/App.js
+++ front-end/playground/src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './PermanentComponents/Header';
 import SidebarComponent from './PermanentComponents/SidebarComponent';
+import Footer from './PermanentComponents/Footer';
+ 
  
 
 function App() {
@@ -9,7 +11,13 @@ function App() {
     <div>
       <Header />
       <SidebarComponent />
-      <h1>Main Content</h1>
+      <h1>
+        Main Content 
+        <br /> <br /> <br /> <br /><br /> <br /> <br /> <br /><br /> <br /> <br /> <br /><br /> <br /> <br /> <br />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        Main Content <br />
+      </h1>
+      <Footer />
     </div>
   );
 }
