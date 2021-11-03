e7361bc253e7304f1630b32aefa5b9f60d8a8896
Musa <musaihtiyar@gmail.com>
2021-11-03 21:02:50 +0300

    Sidebar has been prepared. The color of the header is changed too.

--- front-end/playground/src/App.js
+++ front-end/playground/src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './PermanentComponents/Header';
-
+import SidebarComponent from './PermanentComponents/SidebarComponent';
  
 
 function App() {
   return (
     <div>
       <Header />
+      <SidebarComponent />
+      <h1>Main Content</h1>
     </div>
   );
 }
