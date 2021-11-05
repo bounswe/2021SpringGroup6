c04d6b30830c01df371bee9d7309e40a52bca420
batil <atilberk98@gmail.com>
2021-11-05 21:59:30 +0300

    get endpoint

--- back-end/sports_platform/sports_platform_api/views/user_views.py
+++ back-end/sports_platform/sports_platform_api/views/user_views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.core import serializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from ..models.user_models import User, SportSkillLevel
+from ..serializers.user_serializer import UserSerializer
 
-# We will create endpoints here.
+@api_view(['GET'])
+def get_user(request, user_id):
+    try:    
+        user = User.objects.get(pk=user_id)
+    except User.DoesNotExist:
+        return Response('User id does not exist', status=400)
+    except Exception:
+        return Response('An error occured, please try again later.', status=500)
+    
+    try:
+        sports = User.objects.filter(email='1@s.com')
+        print(type(sports))
+        print(sports)
+    except:
+        pass
+    serialized_user = UserSerializer(user).data
+    # drop last_login field that we  do not use
+    serialized_user.pop('last_login')
+    # add activity stream data
+    serialized_user['@context'] = 'https://schema.org/Person'
+    serialized_user['@id'] = user.user_id
+    return Response(serialized_user,status=200)
