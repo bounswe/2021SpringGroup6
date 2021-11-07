0c7f20906303c918d97d9d12d6ec0510e90f3472
fileames <elifsemabalcioglu@gmail.com>
2021-11-07 09:20:37 +0300

    Register Unfollow Url

--- back-end/sports_platform/sports_platform_api/urls.py
+++ back-end/sports_platform/sports_platform_api/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('users/login', user_views.login),
     path('users/<int:user_id>/follow', user_views.follow_user),
+    path('users/<int:user_id>/unfollow', user_views.unfollow_user),
 ]
