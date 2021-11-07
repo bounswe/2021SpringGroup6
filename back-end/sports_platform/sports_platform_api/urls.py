0397c0895dff62c46aee88b0549a60e9bb8c612d
fileames <elifsemabalcioglu@gmail.com>
2021-11-07 09:12:41 +0300

    Register Follow Url

--- back-end/sports_platform/sports_platform_api/urls.py
+++ back-end/sports_platform/sports_platform_api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('users/login', user_views.login)
+    path('users/login', user_views.login),
+    path('users/<int:user_id>/follow', user_views.follow_user),
 ]
