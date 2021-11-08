a9a3e19b1cd8fc9a31b2253a51b265bea00bfd57
batil <atilberk98@gmail.com>
2021-11-08 19:45:44 +0300

    bug fix

--- back-end/sports_platform/sports_platform_api/tests/test_helper_functions.py
+++ back-end/sports_platform/sports_platform_api/tests/test_helper_functions.py
@@ -2,5 +2,6 @@ from ..controllers.guest import Guest
 
 def create_mock_user(user_info):
     guest= Guest(user_info['identifier'], user_info['password'])
-    guest.register(user_info)
+    user = guest.register(user_info)
+    return user
 
