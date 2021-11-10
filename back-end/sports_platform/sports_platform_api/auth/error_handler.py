a4ea83651f4bea4d0ee5646f7a063a512c1942c9
fileames <elifsemabalcioglu@gmail.com>
2021-11-10 18:25:53 +0300

    Delete Unnecessary Comment

--- back-end/sports_platform/sports_platform_api/auth/error_handler.py
+++ back-end/sports_platform/sports_platform_api/auth/error_handler.py
@@ -1,11 +1,9 @@
 from rest_framework.views import exception_handler
 
 def custom_exception_handler(exc, context):
-    # Call REST framework's default exception handler first,
-    # to get the standard error response.
+    
     response = exception_handler(exc, context)
     if(response.data['detail'].code == 'authentication_failed'):
-        #response.data['code'] = 401
         response.data['message'] = "Invalid token."
         del response.data['detail']
 
