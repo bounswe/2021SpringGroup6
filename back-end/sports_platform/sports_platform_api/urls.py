a9093a7ae9f237c404f22b1f78503c66a690e451
fileames <elifsemabalcioglu@gmail.com>
2021-11-12 19:59:44 +0300

    Add get_follower, get_following Urls

--- back-end/sports_platform/sports_platform_api/urls.py
+++ back-end/sports_platform/sports_platform_api/urls.py
@@ -4,6 +4,8 @@ from .views import *
 urlpatterns = [
     path('users/login', user_views.login),
     path('users/<int:user_id>/follow', user_views.follow_user),
+    path('users/<int:user_id>/following', user_views.get_following),
+    path('users/<int:user_id>/follower', user_views.get_follower),
     path('users/<int:user_id>/unfollow', user_views.unfollow_user),
     path('users', user_views.create_user),
     path('users/<int:user_id>', user_views.get_user),
