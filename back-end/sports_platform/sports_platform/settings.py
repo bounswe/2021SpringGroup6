d0ace7514ee5708656863c11275789aad63240e6
fileames <elifsemabalcioglu@gmail.com>
2021-11-14 11:13:22 +0300

    Add Email Settings

--- back-end/sports_platform/sports_platform/settings.py
+++ back-end/sports_platform/sports_platform/settings.py
@@ -140,3 +140,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
