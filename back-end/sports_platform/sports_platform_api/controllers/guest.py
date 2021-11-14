42697ad129026737ee6ec7446bb330b89c426ac3
fileames <elifsemabalcioglu@gmail.com>
2021-11-14 11:15:31 +0300

    Add Generate Random Password Function

--- back-end/sports_platform/sports_platform_api/controllers/guest.py
+++ back-end/sports_platform/sports_platform_api/controllers/guest.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import authenticate
 from ..models.user_models import User
+import string
+import random
 
 class Guest:
 
@@ -26,3 +28,16 @@ class Guest:
             except Exception as e:
                 raise e
         return user
+
+def generate_random_password():
+
+    characters = list(string.ascii_letters + string.digits + ".*")
+    random.shuffle(characters)
+
+    password = []
+    for i in range(15):
+        password.append(random.choice(characters))
+
+    random.shuffle(password)
+
+    return "".join(password)
