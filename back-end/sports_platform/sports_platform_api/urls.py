1c4568b420b5d724883663d825f9308bff561488
fileames <elifsemabalcioglu@gmail.com>
2021-11-14 16:25:04 +0300

    Fix Syntax

--- back-end/sports_platform/sports_platform_api/urls.py
+++ back-end/sports_platform/sports_platform_api/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('users', user_views.create_user),
     path('users/login', user_views.login),
     path('users/logout', user_views.logout),
-    path('users/recover', user_views.forgot_password)
+    path('users/recover', user_views.forgot_password),
     path('users/<int:user_id>', user_views.get_user)
 ]
