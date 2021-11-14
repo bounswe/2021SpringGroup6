3ed944823cfb943bb58a2666da486b50e9fa530f
batil <atilberk98@gmail.com>
2021-11-14 17:12:19 +0300

    test fix

--- back-end/sports_platform/sports_platform_api/tests/test_update_user.py
+++ back-end/sports_platform/sports_platform_api/tests/test_update_user.py
@@ -20,5 +20,5 @@ class UpdateUserTest(TestCase):
         self.assertEqual(response.json()['message'],'User not logged in.')
 
     def test_success(self):
-        response = self.client.put(self.path,self.update_info, **self.header)
+        response = self.client.put(self.path, data=self.update_info, **self.header)
         self.assertEqual(response.status_code, 200)
