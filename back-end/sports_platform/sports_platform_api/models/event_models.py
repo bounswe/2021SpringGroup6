1a4d4c4fe543dcdee03a21a6a383af565fb2ce2f
fileames <elifsemabalcioglu@gmail.com>
2021-11-28 12:14:39 +0300

    Create Participant, Spectator Models

--- back-end/sports_platform/sports_platform_api/models/event_models.py
+++ back-end/sports_platform/sports_platform_api/models/event_models.py
@@ -21,16 +21,45 @@ class Event(models.Model):
     name = models.CharField(primary_key=True, max_length=100, primary_key=True)
     sport = models.ForeignKey('Sport', on_delete=models.CASCADE)
     organizer = models.ForeignKey('User', on_delete=models.CASCADE)
+    description = models.TextField()
+    
     startDate = models.DateTimeField()
+
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
-    minimumattendeecapacity = models.IntegerField(min_value=0)
-    maximumattendeecapacity = models.IntegerField()
-    maxspectatorcapacity = models.IntegerField()
-    description = models.TextField()
-    minskilllevel = models.IntegerField()
-    maxskilllevel = models.IntegerField()
     city = models.CharField()
     district = models.CharField()
 
+    minimumattendeecapacity = models.IntegerField(min_value=0)
+    maximumattendeecapacity = models.IntegerField()
+    maxspectatorcapacity = models.IntegerField()
+    minskilllevel = models.IntegerField()
+    maxskilllevel = models.IntegerField()
+    
 
+
+class EventParticipants(models.Model):
+    class Meta:
+        db_table = 'event_participants'
+        unique_together = (('user', 'event'),)
+
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    event = models.ForeignKey('Event', on_delete=models.CASCADE)
+
+
+class EventSpectators(models.Model):
+    class Meta:
+        db_table = 'event_spectators'
+        unique_together = (('spectator', 'event'),)
+
+    spectator = models.ForeignKey('User', on_delete=models.CASCADE)
+    event = models.ForeignKey('Event', on_delete=models.CASCADE)
+
+
+class EventParticipationRequesters(models.Model):
+    class Meta:
+        db_table = 'event_spectators'
+        unique_together = (('user', 'event'),)
+
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    event = models.ForeignKey('Event', on_delete=models.CASCADE)
