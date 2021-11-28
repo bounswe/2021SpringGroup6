d5414051274810e3615065f5e92a2744c4bb0e91
fileames <elifsemabalcioglu@gmail.com>
2021-11-28 20:30:23 +0300

    Fix naming

--- back-end/sports_platform/sports_platform_api/validation/event_validation.py
+++ back-end/sports_platform/sports_platform_api/validation/event_validation.py
@@ -11,15 +11,15 @@ class Event(serializers.Serializer):
     latitude = models.DecimalField(max_digits=9, decimal_places=6, required=True)
     longitude = models.DecimalField(max_digits=9, decimal_places=6, required=True)
 
-    minimumattendeecapacity = models.IntegerField(min_value=0, required=True)
-    maximumattendeecapacity = models.IntegerField(min_value=1, required=True)
-    maxspectatorcapacity = models.IntegerField(min_value=0, required=True)
-    minskilllevel = models.IntegerField(min_value=1, max_value=5, required=True)
-    maxskilllevel = models.IntegerField(min_value=1, max_value=5, required=True)
+    minimumAttendeeCapacity = models.IntegerField(min_value=0, required=True)
+    maximumAttendeeCapacity = models.IntegerField(min_value=1, required=True)
+    maxSpectatorCapacity = models.IntegerField(min_value=0, required=True)
+    minSkillLevel = models.IntegerField(min_value=1, max_value=5, required=True)
+    maxSkillLevel = models.IntegerField(min_value=1, max_value=5, required=True)
 
     def validate(self, data):
 
-        if data['minskilllevel'] > data['maxskilllevel']:
+        if data['minSkillSevel'] > data['maxSkillSevel']:
             raise serializers.ValidationError("minSkillLevel should be smaller than or equal to maxSkillLevel")
 
         return data
