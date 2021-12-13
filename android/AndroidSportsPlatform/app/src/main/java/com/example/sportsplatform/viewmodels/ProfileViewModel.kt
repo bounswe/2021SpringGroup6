b3b03d67ef1133b5358423539e23ee55acc40d97
omersuve <omer@shopihq.com>
2021-12-13 18:08:44 +0300

    eventrepo added as parameter to viewmodel. searching button for user and event is written.

--- android/AndroidSportsPlatform/app/src/main/java/com/example/sportsplatform/viewmodels/ProfileViewModel.kt
+++ android/AndroidSportsPlatform/app/src/main/java/com/example/sportsplatform/viewmodels/ProfileViewModel.kt
@@ -11,7 +11,9 @@ import com.example.sportsplatform.util.Coroutines
 import com.example.sportsplatform.util.closeSoftKeyboard
 import com.example.sportsplatform.util.toast
 
-class ProfileViewModel(private val repo: UserRepository) : ViewModel() {
+class ProfileViewModel(
+        private val userRepo: UserRepository,
+        private val eventRepo: EventRepository) : ViewModel() {
 
     var userId: String? = null
     var event: String? = null
@@ -23,39 +25,39 @@ class ProfileViewModel(private val repo: UserRepository) : ViewModel() {
             Intent(view.context, SearchOperationsActivity::class.java).also {
                 view.context.startActivity(it)
             }
-
-            if(!userId.isNullOrEmpty() || !event.isNullOrEmpty()){
-                if(!userId.isNullOrEmpty()){
-                    val currResponse = repo.searchUserProfile(Integer.parseInt(userId!!))
-                    view.context.toast(currResponse.toString())
-                    if (currResponse.isSuccessful) {
-                        val userName = currResponse.body()?.name
-                        if (userName != null) {
-                            view.context.toast(userName)
-                        }
-                    } else {
-                        view.context.toast("Fail")
-                    }
-                }
-                else{
-                    val currResponse = repo.searchUserProfile(Integer.parseInt(userId!!))
-                    view.context.toast(currResponse.toString())
-                    if (currResponse.isSuccessful) {
-                        val userName = currResponse.body()?.name
-                        if (userName != null) {
-                            view.context.toast(userName)
-                        }
-                    } else {
-                        view.context.toast("Fail")
-                    }
-
-                }
-
-            }
-
-
         }
+    }
 
+    fun onSearchButtonClick(view: View) {
+        closeSoftKeyboard(view.context, view)
+
+        Coroutines.main {
+            if(!userId.isNullOrEmpty() || !event.isNullOrEmpty()) {
+                if (!userId.isNullOrEmpty()) {
+                    val currResponse = userRepo.searchUserProfile(Integer.parseInt(userId!!))
+                    view.context.toast(currResponse.toString())
+                    if (currResponse.isSuccessful) {
+                        val userName = currResponse.body()?.name
+                        if (userName != null) {
+                            view.context.toast(userName)
+                        }
+                    } else {
+                        view.context.toast("Fail")
+                    }
+                } else {
+                    val currResponse = eventRepo.findEvent(Integer.parseInt(event!!))
+                    view.context.toast(currResponse.toString())
+                    if (currResponse.isSuccessful) {
+                        val eventName = currResponse.body()?.event
+                        if (eventName != null) {
+                            view.context.toast(eventName)
+                        }
+                    } else {
+                        view.context.toast("Fail")
+                    }
+                }
+            }
+        }
     }
 
     fun onProfileButtonClick(view: View) {
