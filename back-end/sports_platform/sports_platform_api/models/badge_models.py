0b4ee75c0538cff79b4cc7f39e23383c8053f9ac
fileames <elifsemabalcioglu@gmail.com>
2021-12-13 23:28:25 +0300

    Seperate Badge Models

--- back-end/sports_platform/sports_platform_api/models/badge_models.py
+++ back-end/sports_platform/sports_platform_api/models/badge_models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from ..models import User, Event
-
 
 class Badge(models.Model):
     class Meta:
@@ -32,11 +30,21 @@ class Badge(models.Model):
 
 class UserBadges(models.Model):
     class Meta:
-        db_table = 'user_badges'
+        db_table = 'user_given_badges'
+        unique_together = (('user', 'from_user', 'badge'),)
 
-    user = models.ForeignKey('User', related_name='event_badges', on_delete=models.CASCADE)
-    event = models.ForeignKey('Event', related_name='user_badges', on_delete=models.CASCADE)
-    badge = models.ForeignKey('Badge', related_name='given_badges', on_delete=models.CASCADE)
+    user = models.ForeignKey('User', related_name='badges_by_users', on_delete=models.CASCADE)
+    from_user = models.ForeignKey('User', related_name='gave_badges', on_delete=models.CASCADE)
+    badge = models.ForeignKey('Badge', related_name='given_badges_by_users', on_delete=models.CASCADE)
+    date = models.DateTimeField()
     
+class EventBadges(models.Model):
+    class Meta:
+        db_table = 'event_badges'
+        unique_together = (('event', 'badge'),)
+
+    event = models.ForeignKey('Event', related_name='event_badges', on_delete=models.CASCADE)
+    badge = models.ForeignKey('Badge', related_name='given_badges_by_events', on_delete=models.CASCADE)
+    date = models.DateTimeField()
 
 
