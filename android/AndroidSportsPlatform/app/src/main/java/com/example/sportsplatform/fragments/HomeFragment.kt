21210e0f802ad7dcd0c189e8fd1294acff91ecc8
kataparuto <ardabudak98@gmail.com>
2021-12-21 20:11:00 +0300

    - Home screen layout is created(create new event button, logout button and recycle view for events attending and events created - Text view style is created - Tne related viewmodel and viewmodelfactory for homepage are created

--- android/AndroidSportsPlatform/app/src/main/java/com/example/sportsplatform/fragments/HomeFragment.kt
+++ android/AndroidSportsPlatform/app/src/main/java/com/example/sportsplatform/fragments/HomeFragment.kt
@@ -5,19 +5,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.sportsplatform.databinding.FragmentHomeBinding
+import com.example.sportsplatform.viewmodels.HomeViewModel
+import com.example.sportsplatform.viewmodels.HomeViewModelFactory
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.generic.instance
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), KodeinAware {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    override lateinit var kodein: Kodein
+
+    private lateinit var viewModel: HomeViewModel
+    private val factory : HomeViewModelFactory by instance()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        kodein = (requireActivity().applicationContext as KodeinAware).kodein
+        viewModel = ViewModelProvider(this, factory).get(HomeViewModel::class.java)
         return binding.root
     }
 }
