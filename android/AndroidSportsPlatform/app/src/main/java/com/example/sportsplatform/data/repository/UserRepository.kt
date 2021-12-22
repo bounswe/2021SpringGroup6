bf5d16ac7054eca50ffb62018a11664c27111c7e
kataparuto <ardabudak98@gmail.com>
2021-12-22 23:56:47 +0300

    - User information is transferred from AuthViewModel to HomeFragment - Current user's participating events endpoint is integrated - Data class for user's participating events is created

--- android/AndroidSportsPlatform/app/src/main/java/com/example/sportsplatform/data/repository/UserRepository.kt
+++ android/AndroidSportsPlatform/app/src/main/java/com/example/sportsplatform/data/repository/UserRepository.kt
@@ -3,9 +3,7 @@ package com.example.sportsplatform.data.repository
 import com.example.sportsplatform.data.api.UserApi
 import com.example.sportsplatform.data.models.requests.UserRegisterRequest
 import com.example.sportsplatform.data.models.requests.UserRequest
-import com.example.sportsplatform.data.models.responses.TokenResponse
-import com.example.sportsplatform.data.models.responses.UserFollowingResponse
-import com.example.sportsplatform.data.models.responses.UserSearchResponse
+import com.example.sportsplatform.data.models.responses.*
 import retrofit2.Response
 
 class UserRepository(private val api: UserApi) {
@@ -23,5 +21,9 @@ class UserRepository(private val api: UserApi) {
 
     suspend fun searchFollowingUserProfile(userId : Int) : Response<UserFollowingResponse> {
         return api.searchFollowingProfile(userId)
+    }
+
+    suspend fun getUsersParticipatingEvents(token: String, userId: Int): Response<UsersParticipatingEvents> {
+        return api.getUsersParticipatingEvents(token, userId)
     }
 }
--- android/AndroidSportsPlatform/app/src/main/java/com/example/sportsplatform/util/Constants.kt
+++ android/AndroidSportsPlatform/app/src/main/java/com/example/sportsplatform/util/Constants.kt
@@ -4,5 +4,6 @@ object Constants {
 
     const val API_KEY = "123"
     const val TABBAR_COUNT = 3
-
+    const val SHARED_PREFS_USER_ID = "user_id"
+    const val SHARED_PREFS_USER_TOKEN = "user_token"
 }
