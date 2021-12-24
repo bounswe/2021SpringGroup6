baf90afa4cd7efaf44feba5e6f4b7b76a77508a2
omersuve <omer@shopihq.com>
2021-12-24 03:01:28 +0300

    user_id variable name change for a bug.

--- android/AndroidSportsPlatform/app/src/main/java/com/example/sportsplatform/data/models/responses/TokenResponse.kt
+++ android/AndroidSportsPlatform/app/src/main/java/com/example/sportsplatform/data/models/responses/TokenResponse.kt
@@ -3,4 +3,9 @@ package com.example.sportsplatform.data.models.responses
 data class TokenResponse(
     val token: String,
     val user_id : Int
-)
+) {
+    var userId: Int? = null
+    fun fetchUserId() {
+        userId = user_id
+    }
+}
