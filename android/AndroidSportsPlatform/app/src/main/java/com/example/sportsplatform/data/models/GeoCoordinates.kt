c0f02e22fb8cb0873077c7abf86acef34b63880d
kataparuto <ardabudak98@gmail.com>
2022-01-04 02:03:17 +0300

    - Delete unnecessary function in GeoCoordinates.kt

--- android/AndroidSportsPlatform/app/src/main/java/com/example/sportsplatform/data/models/GeoCoordinates.kt
+++ android/AndroidSportsPlatform/app/src/main/java/com/example/sportsplatform/data/models/GeoCoordinates.kt
@@ -4,6 +4,4 @@ data class GeoCoordinates(
     val type: String?,
     val latitude: Double?,
     val longitude: Double?
-) {
-    fun getLatLngTogether() = "lat: $latitude  long: $longitude"
-}
+)
