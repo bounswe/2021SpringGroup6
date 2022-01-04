99617a6f5e153ed87f48487f4a02b7f63697fc2e
Musa <musaihtiyar@gmail.com>
2022-01-04 10:37:10 +0300

    Ultimate Frisbee game has been added to the system for both events and equipments.

--- front-end/playground/src/pages/SearchEquipment/SearchEquipment.js
+++ front-end/playground/src/pages/SearchEquipment/SearchEquipment.js
@@ -242,6 +242,7 @@ class SearchEquipment extends React.Component {
                                 <MenuItem value={'soccer'}>Soccer</MenuItem>                                                                               
                                 <MenuItem value={'table_tennis'}>Table_tennis</MenuItem>
                                 <MenuItem value={'tennis'}>Tennis</MenuItem>
+                                <MenuItem value={'ultimate_frisbee'}>Ultimate-Frisbee</MenuItem>
                                 <MenuItem value={'volleyball'}>Volleyball</MenuItem>
                                 <MenuItem value={'watersports'}>Watersports</MenuItem>
                                 <MenuItem value={'weightlifting'}>Weightlifting</MenuItem>
